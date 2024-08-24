4072627829fde8758aec2f8442859b357a1c9025
qcloud <ubuntu@localhost.localdomain>
2024-08-24 20:07:32 +0800

    new

--- html/christmas/HomeView.vue
+++ html/christmas/HomeView.vue
@@ -21,7 +21,7 @@
             size="100px"
             class="custom-drawer"
             :with-header="false">
-          <el-link style="margin-top:10px; width: 100px;height: 100px" href="https://github.com/PerCheung"
+          <el-link style="margin-top:10px; width: 100px;height: 100px" href="https://github.com/ZhangZhiwei01"
                    target="_blank"
                    :underline="false">
             <img src="@/assets/github.png"
@@ -39,7 +39,7 @@
                  @mouseout="reset_toc"
                  :style="{ width: isHovered_toc ? '100px' : '70px' }"/>
           </el-link>
-          <el-link style="width: 100px;height: 100px" href="https://blog.csdn.net/weixin_43982359" target="_blank"
+          <el-link style="width: 100px;height: 100px" href="https://blog.csdn.net/FDGTNH" target="_blank"
                    :underline="false">
             <img src="@/assets/csdn.png"
                  alt="CSDN"
